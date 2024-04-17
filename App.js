315bf1e8153253e0f1c274bb3ca807fbb1c2f2ac
Isaac J <duckhunterjr@gmail.com>
2024-04-16 23:33:40 -0700

    add expo demo app

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
+//import { Image } from 'expo-image';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, ScrollView,Image,TextInput } from 'react-native';
+//import './global.css';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <ScrollView>
+      <View className='h-40 bg-amber-200'> 
+      <Text className="text-slate-700 text-center text-xl text-bold pt-20 ">The Place for Doggos</Text>
     </View>
+    <Image source={require('./assets/images/dogLogo.png')} />
+      <View className='h-40 bg-amber-500'>
+        <Text className="text-center text-xl">We take care of all kinds of dogs. Come as you are, Woof! </Text>
+        <Text className="text-center text-xl">If you like Peanut Butter, Bones, and Chasing squirrels, This is the kennel club for you!</Text>
+        </View>
+      <View>
+        <TextInput className="border-4 h-10 border-rose-500" value='Enter Name'></TextInput>
+        <TextInput className="border-4 h-10 border-rose-500"  value='Enter Comment'></TextInput>
+        <Text className="border-4 h-10 bg-blue-500 text-center" >Submit</Text>
+        </View>    
+    </ScrollView >
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
